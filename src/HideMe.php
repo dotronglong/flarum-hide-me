dd811305498c78def03663df19012620a555062a
Lucas <me@dotronglong.com>
2019-12-23 03:29:37 +0800

    Improve code

--- src/HideMe.php
+++ src/HideMe.php
@@ -2,11 +2,6 @@
 
 namespace Long\HideMe;
 
-use Flarum\Discussion\Discussion;
-use Flarum\Post\CommentPost;
-use Flarum\Post\Post;
-use Long\HideMe\User\Anonymous;
-
 class HideMe
 {
     const PUBLIC = 1;
@@ -19,19 +14,5 @@ class HideMe
             self::PUBLIC,
             self::ANONYMOUS,
         ]);
-    }
-
-    public static function hide($model)
-    {
-        $anonymous = Anonymous::user();
-        if ($model instanceof Discussion) {
-            $lastPost = $model->lastPost()->first();
-            if ($lastPost[self::COL_HIDE_ME] === self::ANONYMOUS) {
-                $model->setRelation('lastPostedUser', $anonymous);
-            }
-        } else if (($model instanceof Post || $model instanceof CommentPost)
-            && $model[self::COL_HIDE_ME] === self::ANONYMOUS) {
-            $model->setRelation('user', $anonymous);
-        }
     }
 }
--- src/Listener/SerializingListener.php
+++ src/Listener/SerializingListener.php
@@ -7,7 +7,11 @@ use Flarum\Api\Event\Serializing;
 use Flarum\Api\Serializer\BasicPostSerializer;
 use Flarum\Api\Serializer\DiscussionSerializer;
 use Flarum\Api\Serializer\PostSerializer;
+use Flarum\Discussion\Discussion;
+use Flarum\Post\CommentPost;
+use Flarum\Post\Post;
 use Long\HideMe\HideMe;
+use Long\HideMe\User\Anonymous;
 
 class SerializingListener
 {
@@ -19,7 +23,21 @@ class SerializingListener
             if ($event->actor->can('dotronglong-hide-me.see_author')) {
                 return; // no hidden
             }
-            HideMe::hide($event->model);
+            $this->hide($event);
+        }
+    }
+
+    private function hide(Serializing $event)
+    {
+        $anonymous = Anonymous::user();
+        if ($event->model instanceof Discussion) {
+            $lastPost = $event->model->lastPost()->first();
+            if ($lastPost[HideMe::COL_HIDE_ME] === HideMe::ANONYMOUS) {
+                $event->model->setRelation('lastPostedUser', $anonymous);
+            }
+        } else if (($event->model instanceof Post || $event->model instanceof CommentPost)
+            && $event->model[HideMe::COL_HIDE_ME] === HideMe::ANONYMOUS) {
+            $event->model->setRelation('user', $anonymous);
         }
     }
 }
