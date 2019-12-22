979cfb7b7be67a62b0c2b187f8a81745902d4bd4
Lucas <me@dotronglong.com>
2019-12-22 12:27:08 +0800

    Remove hide_me column on discussions

--- migrations/2019_12_19_000000_update_posts_and_discussions_tables.php
+++ migrations/2019_12_19_000000_update_posts_and_discussions_tables.php
@@ -5,19 +5,11 @@ use Illuminate\Database\Schema\Builder;
 
 return [
     'up' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) {
-            $table->tinyInteger('hide_me')->unsigned()->nullable()->default(1);
-        });
         $schema->table('posts', function (Blueprint $table) {
             $table->tinyInteger('hide_me')->unsigned()->nullable()->default(1);
         });
     },
     'down' => function (Builder $schema) {
-        $schema->table('discussions', function (Blueprint $table) {
-            $table->dropColumn([
-                'hide_me',
-            ]);
-        });
         $schema->table('posts', function (Blueprint $table) {
             $table->dropColumn([
                 'hide_me',
