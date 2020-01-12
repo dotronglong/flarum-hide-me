29454762e96e2122764ef509d3183a08f4a9c42f
Lucas <me@dotronglong.com>
2020-01-12 15:25:39 +0800

    Add migration

---
+++ migrations/2020_01_12_000000_update_discussions_tables.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\Builder;
+
+return [
+    'up' => function (Builder $schema) {
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->tinyInteger('hide_me')->unsigned()->nullable()->default(1);
+        });
+    },
+    'down' => function (Builder $schema) {
+        $schema->table('discussions', function (Blueprint $table) {
+            $table->dropColumn([
+                'hide_me',
+            ]);
+        });
+    }
+];
